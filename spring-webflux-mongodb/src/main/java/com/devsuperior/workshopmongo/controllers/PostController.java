a31e6a1b28be5c1f6cedda159ab39dc075193a6e
lucasdanilox <lukastecno@gmail.com>
2024-02-16 22:37:00 -0300

    GET post by id

--- spring-webflux-mongodb/src/main/java/com/devsuperior/workshopmongo/controllers/PostController.java
+++ spring-webflux-mongodb/src/main/java/com/devsuperior/workshopmongo/controllers/PostController.java
@@ -5,6 +5,7 @@ import java.text.ParseException;
 import java.time.Instant;
 import java.util.List;
 
+import com.devsuperior.workshopmongo.dto.UserDTO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,41 +17,20 @@ import org.springframework.web.bind.annotation.RestController;
 import com.devsuperior.workshopmongo.controllers.util.URL;
 import com.devsuperior.workshopmongo.dto.PostDTO;
 import com.devsuperior.workshopmongo.services.PostService;
+import reactor.core.publisher.Mono;
 
 @RestController
 @RequestMapping(value = "/posts")
 public class PostController {
 
-	@Autowired
-	private PostService service;
+    @Autowired
+    private PostService service;
 
-	/*
-	@GetMapping(value = "/{id}")
-	public ResponseEntity<PostDTO> findById(@PathVariable String id) {
-		PostDTO dto = service.findById(id);
-		return ResponseEntity.ok(dto);
-	}
-	
-	@GetMapping(value = "/titlesearch")
-	public ResponseEntity<List<PostDTO>> findByTitle(@RequestParam(value = "text", defaultValue = "") String text) throws UnsupportedEncodingException {
-		text = URL.decodeParam(text);
-		List<PostDTO> list = service.findByTitle(text);
-		return ResponseEntity.ok(list);
-	}
-	
-	@GetMapping(value = "/fullsearch")
-	public ResponseEntity<List<PostDTO>> fullSearch(
-			@RequestParam(value = "text", defaultValue = "") String text,
-			@RequestParam(value = "minDate", defaultValue = "") String minDate,
-			@RequestParam(value = "maxDate", defaultValue = "") String maxDate) throws UnsupportedEncodingException, ParseException {
-		
-		text = URL.decodeParam(text);
-		Instant min = URL.convertDate(minDate, Instant.EPOCH);
-		Instant max = URL.convertDate(maxDate, Instant.now());
-		
-		List<PostDTO> list = service.fullSearch(text, min, max);
-		return ResponseEntity.ok(list);
-	}
+    @GetMapping(value = "/{id}")
+    public Mono<ResponseEntity<PostDTO>> findById(@PathVariable String id) {
+        return service.findById(id)
+                .map(postDTO -> ResponseEntity.ok().body(postDTO));
+    }
 
-	 */
+
 }
--- spring-webflux-mongodb/src/main/java/com/devsuperior/workshopmongo/services/PostService.java
+++ spring-webflux-mongodb/src/main/java/com/devsuperior/workshopmongo/services/PostService.java
@@ -1,32 +1,24 @@
 package com.devsuperior.workshopmongo.services;
 
+import com.devsuperior.workshopmongo.dto.PostDTO;
+import com.devsuperior.workshopmongo.dto.UserDTO;
 import com.devsuperior.workshopmongo.repositories.PostRepository;
+import com.devsuperior.workshopmongo.services.exceptioons.ResourceNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import reactor.core.publisher.Mono;
 
 @Service
 public class PostService {
 
-	@Autowired
-	private PostRepository repository;
+    @Autowired
+    private PostRepository repository;
 
-	/*
-	@Transactional(readOnly = true)
-	public PostDTO findById(String id) {
-		Post post = repository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Recurso não encontrado"));
-		return new PostDTO(post);
-	}
-	
-	public List<PostDTO> findByTitle(String text) {
-		List<PostDTO> result = repository.searchTitle(text).stream().map(x -> new PostDTO(x)).toList();
-		return result;
-	}
-	
-	public List<PostDTO> fullSearch(String text, Instant minDate, Instant maxDate) {
-		maxDate = maxDate.plusSeconds(86400); // 24 * 60 * 60
-		List<PostDTO> result = repository.fullSearch(text, minDate, maxDate).stream().map(x -> new PostDTO(x)).toList();
-		return result;
-	}
 
-	 */
+    public Mono<PostDTO> findById(String id) {
+        return repository.findById(id)
+                .map(PostDTO::new)
+                .switchIfEmpty(Mono.error(new ResourceNotFoundException("id not found")));
+    }
+
 }
